80ce313414f5545e8a6f9334848e7f5a25d3bb11
Jonathan <jn.gz85@gmail.com>
2018-09-03 19:25:24 +1000

    worked

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,56 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './app.css';
-import ReactImage from './react.png';
+
+import SearchBar from './SearchBar';
+import List from './List';
+
 
 export default class App extends Component {
-  state = { username: null };
-
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+  constructor(props){
+    super(props)
+    this.state = {
+      articles:[],
+      newData: [],
+      error: null,
+    }
   }
 
+  componentDidMount() {
+    this.setState({isLoading: true,});
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=9b215697b6e64eee94691526f2a163d3";
+    
+
+
+    axios.get(url)
+    // convert data to json and map what we want
+    .then(json => json.data.articles.map(res => ({
+      author: `${res.author}`,
+      description: `${res.description}`,
+      id: res.source.id
+    })))
+    // copy the data into two arrays (original - articles) and mutated
+    .then(newData => this.setState({articles: newData, newData: newData}))
+    .catch(error => this.setState({error}))
+      
+  }
+
+  filterNames(e){
+    this.state.articles.filter(article => 
+      article.id.toLowerCase().includes(e.target.value.toLowerCase()))
+  }
+
+ 
+
   render() {
-    const { username } = this.state;
+    const { newData, isLoading} = this.state;
+
     return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+      <div className="Card">
+        <div className="header">Article List</div>
+        <SearchBar searchFunc={(e)=> this.filterNames(e)}/>
+        <List articles={newData}/>
       </div>
-    );
+    )
   }
 }
